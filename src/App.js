9a8edc78c0256f755193076898e034c27518ab0b
Noah <artaxnoah@gmail.com>
2021-08-17 13:35:10 -0500

    add redirect if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import HomePage from './pages/homepage/homepage.component'
@@ -15,7 +15,7 @@ import { setCurrentUser } from './redux/user/user.actions'
 
 
 const App = props => {
-  const {setCurrentUser} = props
+  const {setCurrentUser, currentUser} = props
 
   let unsubscribeFromAuth = null
 
@@ -42,21 +42,25 @@ const App = props => {
     }
   }, [])
 
-
+  console.log(props)
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact path='/signin' render={() => currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUpPage />)} />
       </Switch>
     </div>
     )
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser 
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
